059f1df1bd84220ea5213788c8d8514bbfb60d1c
alefemannuel <alefemannuelifrn@gmail.com>
2019-07-05 11:20:36 -0300

    iniciando projeto no git

---
+++ app/models/Atividade.java
@@ -0,0 +1,47 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import QRCode.CreateQR;
+import play.data.validation.Required;
+import play.db.jpa.Blob;
+import play.db.jpa.Model;
+
+@Entity
+public class Atividade extends Model{
+	
+	@Required
+	public String nome;
+	
+	@Required
+	public String descricao;
+	
+	public Blob qrCode;
+	
+	@Required
+	@Temporal(TemporalType.TIME)
+	public Date hrAbertura, hrFechamento;
+	
+	@OneToMany(mappedBy="atividade")
+	public List<Frequencia> frequencias;
+	
+	@ManyToMany(mappedBy="atividades")
+	public List<Usuario> usuarios;
+	
+	@OneToMany(mappedBy="atividade")
+	public List<Solicitacao> solicitacoes;
+	
+	@OneToMany(mappedBy="atividade")
+	public List<Falta> faltas;
+	
+}
---
+++ app/models/Usuario.java
@@ -0,0 +1,38 @@
+package models;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import play.db.jpa.Blob;
+import play.db.jpa.Model;
+
+@Entity
+public class Usuario extends Model {
+
+	public String nome;
+	public String matricula;
+	public String tipoVinculo;
+	public String url_foto_75x100;
+	public String email;
+
+	@OneToMany(mappedBy="usuario")
+	public List<Frequencia> frequencias;
+	
+	@ManyToMany
+	@JoinTable(name="usuarios_atividades")
+	public List<Atividade> atividades;
+	
+	@OneToMany(mappedBy="usuario")
+	public List<Solicitacao> solicitacoes;
+	
+	@OneToMany(mappedBy="usuario")
+	public List<Falta> faltas;
+	
+}
